51ccf456e405f73412a1e2d5cda458fd9e3fa308
Matthew Barnett <mabarnett0@gmail.com>
2012-03-11 12:41:07 -0500

    Quite a lot, recursion code and object navigation cleaned up, removed unneeded classes, wrote main func, screen for deleted users, etc. Still a few kinks but the testing cycles are long.

--- creepjson.php
+++ creepjson.php
@@ -6,76 +6,55 @@ class redditPage {
   public $obj;
   public $posts = array();
   public $authors = array();
-  public $userpage = 0;
+  public $comments = array();
   public $after = '';
+  public $needles = array();
+  public $finds = array();
   
-  function __construct($url) {
+  function __construct($url, $needles = array()) {
     static $lastreq = 0;
     $this->url = $url;
+    $this->needles = $needles;
     echo 'Loading ' . $url . "\n";
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $this->url); // Set url and have curl return page contents on exec
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
     
-    if (($lastreq + 2000000) > microtime()) { // Delay if we have not waited rate limit
-      usleep(($lastreq + 2000000) - microtime());
+    if (($lastreq + $this->ratelimit) > microtime()) { // Delay if we have not waited rate limit
+      usleep(($lastreq + $this->ratelimit) - microtime());
     }
     
     $this->obj = json_decode(curl_exec($curl));
     $lastreq = microtime();
     curl_close($curl);
   }
-}
-
-class subRedditPage extends redditPage {
-  public $posts = array();
   
-  function parseNodes() {
-    foreach($this->obj->data->children as $child) {
-      $this->authors[] = $child->data->author;
-      $this->posts[$child->data->name] = $child->data->title;
-    }
-    $this->authors = array_unique($this->authors);
-    $this->after = $this->obj->data->after;
-    unset($this->obj);
-  }
-  
-  function parsePosts() {
-  	foreach ($this->posts as $key => $post) {
-  		$postId = substr($key, 3);
-  		$this->posts[$postId] = new commentPage('www.reddit.com/comments/' . $postId . '.json');
-  		$this->posts[$postId]->parseNodes();
-  		$this->authors = array_merge($this->authors, $this->posts[$postId]->authors);
+  function parseObj($obj = 0, $top = 1) {
+  	if (!$obj) {
+  		$obj = $this->obj;
   	}
-    $this->authors = array_unique($this->authors);
+  	foreach ($obj as $subObj) {
+  		$this->parseShallowObj($subObj);
+  	}
   }
   
-}
-
-class commentPage extends redditPage {
-	
-	function parseNodes() {
-    foreach($this->obj[1]->data->children as $child) {
-      $this->parseComment($child);
-    }
-  }
-  
-  function parseComment($comment) {
-  	$this->authors[] = $comment->data->author;
-  	if ($comment->data->replies) {
-  		foreach ($comment->data->replies->data->children as $subComment) {
-  			$this->parseComment($subComment);
+  function parseShallowObj($obj = 0, $top = 1) {
+  	if (!$obj) {
+  		$obj = $this->obj;
+  	}
+  	foreach ($obj->data->children as $child) { // Some objects still acting strange, check for kind == t1/t3 maybe?
+  		if (isset($obj->data)) {
+  		  $this->authors[] = $child->data->author;
+  		  if ($child->kind == 't3') { $this->posts[] = $child->data->id; }
+  		  if ($child->kind == 't1') { $this->comments[] = $child->data->id; }
+  		  if (in_array($child->data->subreddit, $this->needles)) { $this->finds[$child->data->id] = $child->data->kind . '_' . $child->data->subreddit; }
+  		  if (isset($child->data->replies) && $child->data->replies) { $this->parseShallowObj($child->data->replies, 0); }
   		}
   	}
-    $this->authors = array_unique($this->authors);
+  	if (isset($obj->data) && $obj->data->after && $top) { $this->after = $obj->data->after; }
+  	$this->authors = array_unique($this->authors);
   }
-	
-}
-
-class userPage extends redditPage {
-	
-	
-	
+  
 }
 
 class subReddit {
@@ -93,18 +72,78 @@ class subReddit {
     $i = 0;
     $count = 0;
     while ($i < $this->scandepth) {
-    	$this->pages[$i] = new subRedditPage(($i ? $this->url . '.json?count=' . $count . '&after=' . $this->pages[$i-1]->after : $this->url . '.json'));
-    	$this->pages[$i]->parseNodes();
-    	$this->pages[$i]->parsePosts();
+    	$this->pages[$i] = new redditPage(($i ? $this->url . '.json?count=' . $count . '&after=' . $this->pages[$i-1]->after : $this->url . '.json'));
+    	$this->pages[$i]->parseShallowObj();
     	$this->authors = array_merge($this->authors, $this->pages[$i]->authors);
+    	foreach ($this->pages[$i]->posts as $post) {
+    		$this->pages[$i]->subPages[$post] = new redditPage('www.reddit.com/comments/' . $post . '.json');
+    		$this->pages[$i]->subPages[$post]->parseObj();
+    		$this->authors = array_merge($this->authors, $this->pages[$i]->authors);
+    	}
+    	if (!$this->pages[$i]->after) { break; }
     	$count += count($this->pages[$i++]->posts);
     }
-    $this->authors = array_unique($this->authors);
+    $this->authors = array_filter(array_unique($this->authors), 'wipeBadUsers');
   }
   
 }
 
-$rp = new subReddit('www.reddit.com/r/Dallas/', 1);
-$rp->scan();
-print_r($rp->authors);
+class user {
+	public $url;
+	public $name;
+	public $pages = array();
+	public $scandepth;
+	public $needles = array();
+	public $finds = array();
+	
+	function __construct($name, $scandepth, $needles) {
+		$this->url = 'www.reddit.com/user/' . $name;
+		$this->name = $name;
+		$this->scandepth = $scandepth;
+		$this->needles = $needles;
+	}
+	
+	function scan() {
+		$i = 0;
+    $count = 0;
+    while ($i < $this->scandepth) {
+    	$this->pages[$i] = new redditPage(($i ? $this->url . '.json?count=' . $count . '&after=' . $this->pages[$i-1]->after : $this->url . '.json'), $this->needles);
+    	$this->pages[$i]->parseShallowObj();
+    	$this->finds = array_merge($this->finds, $this->pages[$i]->finds);
+    	if (!$this->pages[$i]->after) { break; }
+    	$count += count($this->pages[$i]->posts) + count($this->pages[$i++]->comments);
+    }
+	}
+	
+	
+}
+
+function wipeBadUsers($usr) {
+	if (!$usr || $usr == '[deleted]') {
+		return 0;
+	}
+	return 1;
+}
+
+function creep($scandepth, $secscandepth, $subreddits, $needles) {
+	$usrs = array();
+  $finds = array();
+  foreach ($subreddits as $subreddit) {
+  	$rp = new subReddit('www.reddit.com/r/' . $subreddit . '/', $scandepth);
+    $rp->scan();
+    foreach ($rp->authors as $auth) {
+	    $usrs[$auth] = new user($auth, $secscandepth, $needles);
+      $usrs[$auth]->scan();
+      unset($usrs[$auth]->pages);
+      if (count($usrs[$auth]->finds)) {
+  	    $finds[$auth] = $usrs[$auth]->finds;
+      }
+    }
+    unset($rp->pages);
+  }
+  print_r($finds);
+}
+
+creep(10, 10, array('Dallas'), array('guns', 'linux', 'programming', 'Minecraft'));
+
 ?>
